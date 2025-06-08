6cf68cdf597079f4cec1fcd1596416bd3e3d7bc6
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-06-08 15:24:33 +0530

    PatientUpdateRequestDto added

---
+++ src/main/java/com/pm/patientservice/dtos/PatientUpdateRequestDto.java
@@ -0,0 +1,22 @@
+package com.pm.patientservice.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PatientUpdateRequestDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of Birth is required")
+    private String dateOfBirth;
+}
